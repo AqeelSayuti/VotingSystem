8cf8efdc1647c87d442f1cd3f4cee46642134e6e
Dxhxm88 <ahmadaham99@gmail.com>
2022-03-11 01:24:20 +0800

    register and recaptca

--- ajax.php
+++ ajax.php
@@ -4,68 +4,73 @@ $action = $_GET['action'];
 include 'admin_class.php';
 $crud = new Action();
 
-if($action == 'login'){
+if ($action == 'login') {
 	$login = $crud->login();
-	if($login)
+	if ($login)
 		echo $login;
 }
-if($action == 'logout'){
+if ($action == 'register') {
+	$register = $crud->register();
+	if ($register)
+		echo $register;
+}
+if ($action == 'logout') {
 	$logout = $crud->logout();
-	if($logout)
+	if ($logout)
 		echo $logout;
 }
-if($action == 'save_user'){
+if ($action == 'save_user') {
 	$save = $crud->save_user();
-	if($save)
+	if ($save)
 		echo $save;
 }
-if($action == 'save_category'){
+if ($action == 'save_category') {
 	$save = $crud->save_category();
-	if($save)
+	if ($save)
 		echo $save;
 }
-if($action == 'delete_category'){
+if ($action == 'delete_category') {
 	$delete = $crud->delete_category();
-	if($delete)
+	if ($delete)
 		echo $delete;
 }
-if($action == 'save_voting'){
+if ($action == 'save_voting') {
 	$save = $crud->save_voting();
-	if($save)
+	if ($save)
 		echo $save;
 }
-if($action == 'get_voting'){
+if ($action == 'get_voting') {
 	$get = $crud->get_voting();
-	if($get)
+	if ($get)
 		echo $get;
 }
-if($action == 'update_voting'){
+if ($action == 'update_voting') {
 	$update = $crud->update_voting();
-	if($update)
+	if ($update)
 		echo $update;
 }
-if($action == 'delete_voting'){
+if ($action == 'delete_voting') {
 	$delete = $crud->delete_voting();
-	if($delete)
+	if ($delete)
 		echo $delete;
 }
-if($action == 'save_opt'){
+if ($action == 'save_opt') {
 	$save = $crud->save_opt();
-	if($save)
+	if ($save)
 		echo $save;
 }
-if($action == 'delete_candidate'){
+if ($action == 'delete_candidate') {
 	$delete = $crud->delete_candidate();
-	if($delete)
+	if ($delete)
 		echo $delete;
 }
-if($action == 'save_settings'){
+if ($action == 'save_settings') {
 	$save = $crud->save_settings();
-	if($save)
+	if ($save)
 		echo $save;
 }
-if($action == 'submit_vote'){
+if ($action == 'submit_vote') {
 	$save = $crud->submit_vote();
-	if($save)
+	if ($save)
 		echo $save;
 }
